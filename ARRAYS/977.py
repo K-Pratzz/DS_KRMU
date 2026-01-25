de706d280c3fe13fd0aa4175fff69ddc14e2fef4
Kumari Pratibha <kp939331@gmail.com>
2026-01-25 15:19:20 +0530

    997

---
+++ ARRAYS/977.py
@@ -0,0 +1,26 @@
+'''Given an integer array nums sorted in non-decreasing order,
+ return an array of the squares of each number sorted in non-decreasing order.
+
+Example 1:
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].'''
+
+nums=[int(x) for x in input("enter the list of numbers :").split()]
+nums.sort()
+
+def sorted_squares(nums):
+
+    result=[0]*len(nums)
+    left,right=0,len(nums)-1
+    
+    for i in range(len(nums)-1,-1,-1):
+        if abs(nums[left])>abs(nums[right]):
+            result[i]=nums[left]**2
+            left+=1
+        else:
+            result[right]=nums[right]**2
+            right-=1
+    return result
+print(sorted_squares(nums))
